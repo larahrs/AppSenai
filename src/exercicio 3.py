382c2897994487cc7fe653abf9ee56cfb15acade
senai_25_26 <larinhaasilva09@gmail.com>
2026-03-31 14:13:30 -0300

    github

--- src/exercicio 3.py
+++ src/exercicio 3.py
@@ -20,8 +20,9 @@ def main(page: flet.Page):
     def salvar_funcionario():
         text_nome.value = input_nome.value
         text_cpf.value = input_cpf.value
-        text_email.value = input_email.value
         text_salario.value = input_salario.value
+        text_numero_clientes.value = input_numero_clientes.value
+        text_regiao_trabalho.value = input_regiao_trabalho.value
 
         tem_erro = False
 
@@ -37,23 +38,30 @@ def main(page: flet.Page):
             tem_erro = True
             input_cpf.error = "campo obrigatório"
 
-        if input_email.value:
-            input_email.error = None
-        else:
-            tem_erro = True
-            input_email.error = "campo obrigatório"
-
         if input_salario.value:
             input_salario.error = None
         else:
             tem_erro = True
             input_salario.error = "campo obrigatório"
 
+        if input_numero_clientes.value:
+            input_numero_clientes.error = None
+        else:
+            tem_erro = True
+            input_numero_clientes.error = "campo obrigatório"
+
+        if input_regiao_trabalho.value:
+            input_regiao_trabalho.error = None
+        else:
+            tem_erro = True
+            input_regiao_trabalho.error = "campo obrigatório"
+
         if not tem_erro:
             input_nome.value = ""
             input_cpf.value = ""
-            input_email.value = ""
             input_salario.value = ""
+            input_numero_clientes.value = ""
+            input_regiao_trabalho.value = ""
             navegar("/segunda_tela")
 
     # Gerenciar as telas (routes)
@@ -64,8 +72,8 @@ def main(page: flet.Page):
                 route="/",
                 controls=[
                     AppBar(
-                        title="funcionário",
-                        bgcolor=Colors.CYAN_700
+                        title="fornecedor",
+                        bgcolor=Colors.BLUE_900
                     ),
                     Text("digite seu nome:"),
                     input_nome,
@@ -73,11 +81,14 @@ def main(page: flet.Page):
                     Text("digite seu CPF:"),
                     input_cpf,
 
-                    Text("digite seu Email:"),
-                    input_email,
-
-                    Text("digite seu salário:"),
+                    Text("digite seu salario:"),
                     input_salario,
+
+                    Text("digite seu numero de clientes:"),
+                    input_numero_clientes,
+
+                    Text("digite sua regiao de trabalho:"),
+                    input_regiao_trabalho,
                     OutlinedButton("Salvar", on_click=salvar_funcionario)
                 ]
             )
@@ -88,13 +99,14 @@ def main(page: flet.Page):
                     route="/segunda_tela",
                     controls=[
                         AppBar(
-                            title="funcionários",
-                            bgcolor=Colors.CYAN_700
+                            title="fornecedor",
+                            bgcolor=Colors.BLUE_900
                         ),
                         text_nome,
                         text_cpf,
-                        text_email,
                         text_salario,
+                        text_numero_clientes,
+                        text_regiao_trabalho,
                     ]
                 )
             )
@@ -110,12 +122,14 @@ def main(page: flet.Page):
     text = Text()
     input_nome = TextField(label="nome")
     input_cpf = TextField(label="CPF")
-    input_email = TextField(label="email")
     input_salario = TextField(label="salario")
+    input_numero_clientes = TextField(label="numero de clientes")
+    input_regiao_trabalho = TextField(label="regiao de trabalho")
     text_nome = TextField(label="nome")
-    text_cpf = TextField(label="CPF")
-    text_email = TextField(label="email")
+    text_cpf = TextField(label="cpf")
     text_salario = TextField(label="salario")
+    text_numero_clientes = TextField(label="numero de clientes")
+    text_regiao_trabalho = TextField(label="regiao de trabalho")
 
     # Eventos
     page.on_route_change = route_change
